09706fb20b2a90e46874f73fe800b7ef1021d4b3
Betsy Lorton and Will Strinz <dev+schbetsy+wstrinz@bendyworks.com>
2015-06-23 11:01:02 -0500

    working widget display

--- frontend/app/models/widget.js
+++ frontend/app/models/widget.js
@@ -6,5 +6,10 @@ export default DS.Model.extend({
   locationX: DS.attr('number'),
   locationY: DS.attr('number'),
   width: DS.attr('number'),
-  height: DS.attr('number')
+  height: DS.attr('number'),
+  style: function() {
+    return "top: " + this.get('locationY') + "px; left: " + this.get('locationX') +
+           "px; height: " + this.get('height') + "px; width: " +
+           this.get('width') + "px; border: 1px solid; position: absolute; overflow: hidden;";
+  }.property('width', 'height', 'locationY', 'locationX')
 });

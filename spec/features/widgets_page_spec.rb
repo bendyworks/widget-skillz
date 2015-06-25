0210475ab032976e60dd465c0b8e964f0cfed01f
Betsy Lorton and Will Strinz <dev+schbetsy+wstrinz@bendyworks.com>
2015-06-25 09:21:20 -0500

    fix jquery-ui loading

--- spec/features/widgets_page_spec.rb
+++ spec/features/widgets_page_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 feature "Widgets page", type: :feature, js: true do
   scenario "User visits widget page" do
-    begin
       visit "/widgets"
-    rescue
-      binding.pry
-      puts "wrong line"
-    end
-      binding.pry
     expect(page).to have_text("Ember")
   end
 end
